e50e259d36118d52f2b3a70bea131bd00ca68948
p1pp0land1n3 <33880971+p1pp0land1n3@users.noreply.github.com>
2017-11-21 22:38:27 +0000

    Add files via upload

---
+++ themes/vVide/languages/ThemeStrings.english.php
@@ -0,0 +1,8 @@
+<?php
+// Version: 2.0; Themes
+
+global $scripturl;
+$txt['forum_search'] = 'Search...';
+$txt['wt_copyright'] = '<span>Design by <a href="http://webtiryaki.com/" target="_blank">Webtiryaki</a></span>';
+
+?>
